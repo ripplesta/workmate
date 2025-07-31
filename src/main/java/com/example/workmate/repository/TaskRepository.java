ad926e416c2bb169af91e8c5ef2c385435af0c89
ripplesta <ripple1192@yahoo.co.jp>
2025-07-31 15:51:11 +0900

    TaskRepository.java を更新

--- src/main/java/com/example/workmate/repository/TaskRepository.java
+++ src/main/java/com/example/workmate/repository/TaskRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.workmate.domain.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-	List<Task> findByUserId(Long userId);
+	List<Task> findByUserId(Account account);
 
 }
